abdb41751ece0e103dd2a68634939558e6dae8e7
Lloyd <lloyd.williams1062@gmail.com>
2025-05-08 00:45:48 +1000

    🙃

--- flake.nix
+++ flake.nix
@@ -17,44 +17,28 @@
       inputs.hyprland.follows = "hyprland";
     };
   };
-  outputs =
-    inputs@{
-      nixpkgs,
-      home-manager,
-      lim,
-      ...
-    }:
+  outputs = inputs@{ nixpkgs, home-manager, lim, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        config = {
-          allowUnfree = true;
-        };
+        config = { allowUnfree = true; };
       };
-    in
-    {
+    in {
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           system = system;
           modules = [
-            (
-              { config, pkgs, ... }:
-              {
-                nixpkgs.config.allowUnfree = true;
-              }
-            )
+            ({ config, pkgs, ... }: { nixpkgs.config.allowUnfree = true; })
             ./configuration.nix
             home-manager.nixosModules.home-manager
             {
               home-manager.extraSpecialArgs = { inherit inputs pkgs system; };
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
               home-manager.users.lloyd = {
-                imports = [
-                  ./home.nix
-                  lim.home-manager-module
-                ];
+                imports = [ ./home.nix lim.home-manager-module ];
               };
             }
           ];
