d613ab6f91880e45411411ecfa3949b7e880ec1a
Lloyd <lloyd.williams1062@gmail.com>
2025-05-17 15:45:30 +1000

    🙃

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,6 @@
     substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
-  services.kidex.enable = true;
 
   # Bluetooth
   hardware.bluetooth.enable = true;
