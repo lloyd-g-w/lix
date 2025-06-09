92ade08507f8db2ba770dea18ef060f3eae5320f
Lloyd <lloyd.williams1062@gmail.com>
2025-06-09 19:53:12 +1000

    🙃

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
     ];
   };
 
+  inputs.self.submodules = true;
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
