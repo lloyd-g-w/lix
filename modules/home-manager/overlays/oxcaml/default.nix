b8a3105f35db99b5f6cda2ed3dbfc81bf8ce0adc
Lloyd <lloyd.williams1062@gmail.com>
2025-07-06 01:07:24 +1000

    🙃

--- modules/home-manager/overlays/oxcaml/default.nix
+++ modules/home-manager/overlays/oxcaml/default.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}:
 pkgs.recurseIntoAttrs {
   oxcaml = pkgs.callPackage ./oxcaml.nix {};
-  ocamlformat = pkgs.callPackage ./ocaml-format.nix {};
+  ocamlformat = pkgs.callPackage ./ocamlformat.nix {};
   ocaml-lsp = pkgs.callPackage ./ocaml-lsp.nix {};
 }
