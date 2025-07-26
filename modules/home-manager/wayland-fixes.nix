1229d69980fb00fdc49c757efc42799529acff59
Lloyd <lloyd.williams1062@gmail.com>
2025-07-26 22:26:33 +1000

    🙃

--- modules/home-manager/wayland-fixes.nix
+++ modules/home-manager/wayland-fixes.nix
@@ -6,7 +6,7 @@
     src = pkgs.fetchFromGitHub {
       owner = "Rush";
       repo = "wayland-push-to-talk-fix";
-      rev = "master";
+      rev = "490f43054453871fe18e7d7e9041cfbd0f1d9b7d";
       sha256 = "11zbqz9zznzncf84jrvd5hl2iig6i1cpx6pwv02x2dg706ns0535";
     };
 
