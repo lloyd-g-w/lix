1c0f4b2e50651c327b68020208c53deb938c9351
Lloyd <lloyd.williams1062@gmail.com>
2025-07-29 13:43:34 +1000

    🙃

--- home/users/lloyd/desktop.nix
+++ home/users/lloyd/desktop.nix
@@ -2,5 +2,5 @@
   imports = [./common.nix];
 
   # Setup hyprland monitors with custom option
-  lix.hyprland.monitors = ["DP-1, preferred, 0x0, 1" "HDMI-A-2, preferred, auto, 1"];
+  lix.hyprland.monitors = ["DP-4, preferred, 0x0, 1" "HDMI-A-2, preferred, auto, 1"];
 }
