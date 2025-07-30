2ca1b3d57a22b6d92d348601ac72a45de5b7fc4d
Lloyd <lloyd.williams1062@gmail.com>
2025-07-30 15:29:53 +1000

    🙃

--- home/users/lloyd/desktop.nix
+++ home/users/lloyd/desktop.nix
@@ -2,5 +2,8 @@
   imports = [./common.nix];
 
   # Setup hyprland monitors with custom option
-  lix.hyprland.monitors = ["DP-4, preferred, 0x0, 1" "HDMI-A-2, preferred, auto, 1"];
+  lix.hyprland.monitors = [
+    "desc:Samsung Electric Company LC27G5xT HNAW600500, preferred, 0x0, 1"
+    "desc:BNQ BenQ XL2411Z C6F00870SL0, preferred, auto, 1"
+  ];
 }
