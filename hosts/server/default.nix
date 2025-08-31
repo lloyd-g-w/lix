56e0a958ef8decd81ec12faa36d8f07d8737373f
Lloyd <lloyd.williams1062@gmail.com>
2025-08-31 18:04:06 +1000

    🙃

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -30,6 +30,30 @@
     };
   };
 
-  networking.firewall.allowedTCPPorts = [80 5173];
+  virtualisation.podman.enable = false;
+  virtualisation.docker.enable = true;
+  virtualisation.oci-containers.backend = "docker";
+  users.users.lloyd.extraGroups = ["docker"];
+
+  # make persistent dirs for volumes
+  systemd.tmpfiles.rules = [
+    "d /var/lib/nginx-proxy-manager 0755 root root - -"
+    "d /var/lib/nginx-proxy-manager/data 0755 root root - -"
+    "d /var/lib/nginx-proxy-manager/letsencrypt 0755 root root - -"
+  ];
+
+  # run NPM as a declarative OCI container
+  virtualisation.oci-containers.containers.nginx-proxy-manager = {
+    image = "jc21/nginx-proxy-manager:latest";
+    ports = ["80:80" "81:81" "443:443"];
+    autoRemoveOnStop = false;
+    volumes = [
+      "/var/lib/nginx-proxy-manager/data:/data"
+      "/var/lib/nginx-proxy-manager/letsencrypt:/etc/letsencrypt"
+    ];
+    extraOptions = ["--restart=unless-stopped"];
+  };
+
+  networking.firewall.allowedTCPPorts = [443 2020 80 81 5173 7070];
   programs.zsh.enable = true;
 }
