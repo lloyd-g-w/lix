ff84d7c78243ba3d0d55a8eadd6718e3ad761bad
Lloyd <lloyd.williams1062@gmail.com>
2025-11-17 23:38:28 +1100

    🙃

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -14,6 +14,7 @@
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  services.desktopManager.cosmic.enable = true;
 
   nix.settings.trusted-users = [
     "root"
