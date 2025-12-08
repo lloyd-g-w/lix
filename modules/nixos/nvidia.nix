d8d6948cdc3aeedeae8e37af18ad18fa3b658dde
Lloyd <lloyd.williams1062@gmail.com>
2025-12-08 13:16:01 +1100

    🙃

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -3,11 +3,10 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-
     extraPackages = [pkgs.nvidia-vaapi-driver];
   };
 
   services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia.open = false;
+  hardware.nvidia.open = true;
 }
