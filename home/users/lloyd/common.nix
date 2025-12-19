0b51ecf50edbecef6d64bb93c8a0ee544f4677f1
Lloyd <lloyd.williams1062@gmail.com>
2025-12-19 21:34:53 +1100

    🙃

--- home/users/lloyd/common.nix
+++ home/users/lloyd/common.nix
@@ -18,6 +18,7 @@
 
   home.packages = with pkgs; [
     # Utils
+    kdePackages.filelight # disk usage gui
     unzip
     atool
     httpie
--- home/users/server/default.nix
+++ home/users/server/default.nix
@@ -18,6 +18,7 @@
 
   home.packages = with pkgs; [
     # Utils
+    ncdu # disk usage tui
     unzip
     atool
     httpie
