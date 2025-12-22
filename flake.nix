f2c810126baad9fe964c4a96668b9777d0dcbd05
Lloyd <lloyd.williams1062@gmail.com>
2025-12-22 23:03:02 +1100

    🙃

--- flake.nix
+++ flake.nix
@@ -24,35 +24,25 @@
       url = "github:lloyd-g-w/latus";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    lix-config.url = "path:./config.nix";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
 
-      imports =
-        [
-          inputs.home-manager.flakeModules.home-manager
-          ./options # This exposes the avaliable lix options
-          ./nixos
-          ./home
-          ./overlays
-          ./pkgs
-          ./shells
-          ./templates
-        ]
-        ++ (
-          let
-            # This configPath is where you define the lix options you want
-            # this needs to be bootstrapped intially (e.g. LIX_DIR="DIR" sudo home-manager rebuild switch ...)
-            # but then is set by home manager and is defined in your config
-            lixDir = builtins.getEnv "LIX_DIR";
-            configPath = /. + (lixDir + "/config.nix");
-          in
-            if lixDir != "" && builtins.pathExists configPath
-            then [configPath]
-            else []
-        );
+      imports = [
+        inputs.home-manager.flakeModules.home-manager
+        inputs.lix-config.flakeModules.default # This configPath is where you define the lix options you want
+        ./options # This exposes the avaliable lix options
+        ./nixos
+        ./home
+        ./overlays
+        ./pkgs
+        ./shells
+        ./templates
+      ];
 
       lix.home.sharedModules = [
         inputs.walker.homeManagerModules.default
