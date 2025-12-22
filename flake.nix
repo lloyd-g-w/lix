b29d9e2b8b19bc98bda3a4f17e70d4cea517ffb1
Lloyd <lloyd.williams1062@gmail.com>
2025-12-22 22:15:33 +1100

    🙃

--- flake.nix
+++ flake.nix
@@ -30,17 +30,23 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
 
-      imports = [
-        inputs.home-manager.flakeModules.home-manager
-        ./config.nix # This is where you define the lix options you want
-        ./options # This exposes the avaliable lix options
-        ./nixos
-        ./home
-        ./overlays
-        ./pkgs
-        ./shells
-        ./templates
-      ];
+      imports =
+        [
+          inputs.home-manager.flakeModules.home-manager
+          ./options # This exposes the avaliable lix options
+          ./nixos
+          ./home
+          ./overlays
+          ./pkgs
+          ./shells
+          ./templates
+        ]
+        ++ (
+          # This is where you define the lix options you want
+          if builtins.pathExists ./config.nix
+          then [./config.nix]
+          else []
+        );
 
       lix.home.sharedModules = [
         inputs.walker.homeManagerModules.default
